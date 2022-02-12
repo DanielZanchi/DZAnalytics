7dc205e93302e284e7ffcd83b3184e8689b1615f
Daniel Zanchi <dev@danielzanchi.com>
2022-02-12 17:09:23 +0100

    get local receipt

--- Sources/DZDataAnalytics/DZAnalytics.Events.swift
+++ Sources/DZDataAnalytics/DZAnalytics.Events.swift
@@ -47,7 +47,7 @@ extension DZDataAnalytics {
         ])
     }
     
-    public func sendReceiptInfos(_ receipt: [String: AnyObject]) {
+    public func sendReceiptInfos(_ receipt: [String: AnyObject], localReceipt: Bool = false) {
         guard AnalyticsVars.didSendReceipt == false else { return }
         if let receiptInfo = receipt["receipt"] {
             if let originalPurchaseDate = receiptInfo["original_purchase_date"] as? String,
@@ -56,7 +56,8 @@ extension DZDataAnalytics {
                 sendEvent(withName: "ce_app_store_receipt", parameters: [
                     "cp_r_original_purchase_date": originalPurchaseDate,
                     "cp_r_download_id": downloadId,
-                    "cp_r_original_application_version": originalAppVersion
+                    "cp_r_original_application_version": originalAppVersion,
+                    "cp_r_local_receipt": localReceipt
                 ])
                 
                 AnalyticsVars.didSendReceipt = true
@@ -65,16 +66,16 @@ extension DZDataAnalytics {
         }
     }
     
-    public func fetchAndSendReceipt(withSharedKey sharedKey: String, env: AppleReceiptValidator.VerifyReceiptURLType = .production) {
+    public func getAndSendReceipt(withSharedKey sharedKey: String, env: AppleReceiptValidator.VerifyReceiptURLType = .production, forceRefresh: Bool = false) {
         guard AnalyticsVars.didSendReceipt == false else { return }
-        SwiftyStoreKit.fetchReceipt(forceRefresh: false) { result in
+        SwiftyStoreKit.fetchReceipt(forceRefresh: forceRefresh) { result in
             switch result {
             case .success(let receiptData):
                 let validator = AppleReceiptValidator(service: env, sharedSecret: sharedKey)
                 validator.validate(receiptData: receiptData) { result in
                     switch result {
                     case .success(let receipt):
-                        self.sendReceiptInfos(receipt)
+                        self.sendReceiptInfos(receipt, localReceipt: !forceRefresh)
                     case .error(let error):
                         print("There was an error fetching the receipt: \(error.localizedDescription)")
                     }
