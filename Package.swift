f123a215f647f00c7d853bbd940e5a4c457fe984
Daniel Zanchi <danny.zanchi@gmail.com>
2022-04-07 15:48:57 +0200

    update to iOS 14

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "DZDataAnalytics",
+    name: "DZDatsaAnalytics",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v14)
     ],
     products: [
         .library(
