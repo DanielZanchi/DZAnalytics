e1f017c9396557a9fc1fd2e0c2b099b5b6c16eea
Daniel Zanchi <danny.zanchi@gmail.com>
2022-04-07 22:15:45 +0200

    updated package

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", .exactItem("8.13.0")),
-//        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", range: "8.14.0"),
         .package(name: "SwiftyStoreKit", url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.4"),
         .package(name: "SwiftKeychainWrapper", url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1")
     ],
