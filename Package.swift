f9a8fa3e56fbd157a19664ccb4e486493eabd2d7
Daniel Zanchi <danny.zanchi@gmail.com>
2022-05-31 10:06:57 +0200

    updated firebase

--- Package.swift
+++ Package.swift
@@ -14,9 +14,9 @@ let package = Package(
             targets: ["DZDataAnalytics"]),
     ],
     dependencies: [
-        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", .exactItem("8.14.0")),
         .package(name: "SwiftyStoreKit", url: "https://github.com/bizz84/SwiftyStoreKit.git", from: "0.16.4"),
-        .package(name: "SwiftKeychainWrapper", url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1")
+        .package(name: "SwiftKeychainWrapper", url: "https://github.com/jrendel/SwiftKeychainWrapper", from: "4.0.1"),
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", branch: "master")
     ],
     targets: [
         .target(
